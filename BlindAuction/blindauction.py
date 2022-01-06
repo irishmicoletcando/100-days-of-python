b3a6c85d93456c47fdd75483df37f8f864e570d6
Irish Micole Cando <73147283+irishmicoletcando@users.noreply.github.com>
2022-01-06 07:28:49 +0000

    Defined a function for the auction program

--- BlindAuction/blindauction.py
+++ BlindAuction/blindauction.py
@@ -2,3 +2,21 @@ from replit import clear
 #HINT: You can call clear() to clear the output in the console.
 from art import logo
 print(logo)
+
+def blind_auction_program():
+  name = input("What is your name? ")
+  while True:
+    try:
+      bid_price = float(input("What's your bid? $"))
+    except ValueError:
+      print("Invalid. Please enter a number")
+    else:
+      break
+
+  auction = {}
+  auction["name"] = name
+  auction["bidprice"] = bid_price
+  print(auction)
+
+
+blind_auction_program()
