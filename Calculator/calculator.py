b0d3655cfe68d34bb72b1b0327e0326217051967
Irish Micole Cando <73147283+irishmicoletcando@users.noreply.github.com>
2022-01-10 23:21:00 +0000

    Added recursion of the whole program

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,8 +1,6 @@
 from art import logo
 print(logo)
 
-second_trial = False
-
 def calculator_operation(num1, num2, operation_inputted):
   if operation_inputted == "+":
     answer = num1 + num2
@@ -17,32 +15,36 @@ def calculator_operation(num1, num2, operation_inputted):
     answer = num1 / num2
     print(f"{num1} / {num2} = {answer}")
 
-while True:
-  try:
-    first_number = float(input("What is your first number? "))
-  except ValueError:
-    print("Enter a number.")
-  else:
-    break
-
-while not second_trial:
-  print("+\n-\n*\n/")
-  operation = input("Pick an operation: ")
-
+def calculator_program():
+  second_trial = False
   while True:
     try:
-      second_number = float(input("What is the second number? "))
+      first_number = float(input("What is your first number? "))
     except ValueError:
       print("Enter a number.")
     else:
       break
 
-  calculator_operation(num1=first_number, num2=second_number, operation_inputted=operation)
+  while not second_trial:
+    print("+\n-\n*\n/")
+    operation = input("Pick an operation: ")
 
-  yes_or_no = input("Type 'y' to continue with the result, or type 'n' to exit. ").lower()
-  if yes_or_no == "n":
-    second_trial = True
-    print("Goodbye!")
-    break
+    while True:
+      try:
+        second_number = float(input("What is the second number? "))
+      except ValueError:
+        print("Enter a number.")
+      else:
+        break
 
-  
+    calculator_operation(num1=first_number, num2=second_number, operation_inputted=operation)
+
+    yes_or_no = input("Type 'y' to continue with the result, or type 'n' to exit. ").lower()
+    if yes_or_no == "n":
+      second_trial = True
+      calculator_program()
+
+
+calculator_program()
+
+    
