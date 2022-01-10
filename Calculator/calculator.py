0592847992aa9210729824a0e64a80869763f28e
Irish Micole Cando <73147283+irishmicoletcando@users.noreply.github.com>
2022-01-10 12:20:22 +0000

    Added user inputs

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,2 +1,21 @@
 from art import logo
 print(logo)
+
+while True:
+  try:
+    first_number = float(input("What is your first number? "))
+  except ValueError:
+    print("Enter a number.")
+  else:
+    break
+
+print("+\n-\n*\n/")
+operation = input("Pick an operation: ")
+
+while True:
+  try:
+    second_number = float(input("What is the second number? "))
+  except ValueError:
+    print("Enter a number.")
+  else:
+    break
