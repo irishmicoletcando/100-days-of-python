65e937ba9c4d2a5bf59093135165528a65ecf5bd
Irish Micole Cando <73147283+irishmicoletcando@users.noreply.github.com>
2022-01-10 00:42:37 +0000

    Added if else statements to display the days

--- DaysInMonth/daysinmonth.py
+++ DaysInMonth/daysinmonth.py
@@ -10,9 +10,14 @@ def is_leap(year):
   else:
     return False
 
-def days_in_month():
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
-  
+def days_in_month(year_inputted, month_inputted):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+  year_inputted = is_leap(year)
+  if year_inputted and month_inputted == 2:
+      return 29
+  else: 
+    month_check = month_inputted - 1 
+  return month_days[month_check]
   
 #🚨 Do NOT change any of the code below 
 year = int(input("Enter a year: "))
